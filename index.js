2a3a6db38c1dafbfa41d6479bee1d70ef3601bd1
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-10-24 20:54:13 +0600

    admin code added

--- index.js
+++ index.js
@@ -19,12 +19,28 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UNAUTHORIZED access!" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "FORBIDDEN access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
     await client.connect();
 
     const bookCollection = client.db("boi_exchange").collection("books");
+    const userCollection = client.db("boi-exchange").collection("users");
+    console.log(userCollection)
 
     app.get("/books", async (req, res) => {
       const query = {};
@@ -34,12 +50,66 @@ async function run() {
     });
 
     app.get("/book/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await bookCollection.findOne(query);
-        res.send(result);
-      });
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookCollection.findOne(query);
+      res.send(result);
+    });
 
+    app.get("/user", verifyJWT, async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
+
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      res.send(user);
+    });
+
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "FORBIDDEN Access" });
+      }
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const user = await userCollection.findOne({ email: email });
+      console.log(user);
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
   } finally {
   }
 }
